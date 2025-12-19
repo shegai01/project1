0cde6f66f5df83cd2c8c7f8620b84e4e49b77f3a
Aleksandr_Sh. <a.shegai91@gmail.com>
2025-12-19 12:24:24 +0300

    refactored

--- cmd/main.go
+++ cmd/main.go
@@ -17,9 +17,12 @@ func main() {
 
 	r := mux.NewRouter()
 	r.Use(internal.MWRecoverPanic)
+
 	h := internal.New(logger, r, &internal.Storage{})
+
 	r.HandleFunc("/links", h.GetStatus)
 	r.HandleFunc("/getbyid", h.GetbyID)
+
 	log.Fatal(http.ListenAndServe(":8080", r))
 
 }
